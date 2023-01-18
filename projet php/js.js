1cfc3ec2dd78d9eb56363e6f4bd130e5412918d2
KhouloudBolif <116397377+KhouloudBolif@users.noreply.github.com>
2023-01-18 20:19:14 +0100

    Add files via upload

---
+++ projet php/js.js
@@ -0,0 +1,51 @@
+function openNav0() {
+    document.getElementById("food").style.width = "45%";
+    document.getElementById("books").style.width="0%";
+    document.getElementById("Vetement").style.width="0%";
+    document.getElementById("electro").style.width="0%";
+    document.getElementById("home").style.width="0%";
+  }
+  function closeNav0() {
+    document.getElementById("food").style.width="0%";
+  }
+  function openNav1() {
+    document.getElementById("Vetement").style.width = "45%";
+    document.getElementById("food").style.width="0%";
+    document.getElementById("books").style.width="0%";
+    document.getElementById("electro").style.width="0%";
+    document.getElementById("home").style.width="0%";
+  }
+  function closeNav1() {
+    document.getElementById("Vetement").style.width="0%";
+    
+  }
+  function openNav2() {
+    document.getElementById("books").style.width = "45%";
+    document.getElementById("food").style.width="0%";
+    document.getElementById("Vetement").style.width="0%";
+    document.getElementById("electro").style.width="0%";
+    document.getElementById("home").style.width="0%";
+  }
+  function closeNav2() {
+    document.getElementById("books").style.width="0%";
+  }
+  function openNav3() {
+    document.getElementById("electro").style.width = "45%";
+    document.getElementById("food").style.width="0%";
+    document.getElementById("Vetement").style.width="0%";
+    document.getElementById("home").style.width="0%";
+    document.getElementById("books").style.width="0%";
+  }
+  function closeNav3() {
+    document.getElementById("electro").style.width="0%";
+  }
+  function openNav4() {
+    document.getElementById("home").style.width = "60%";
+    document.getElementById("food").style.width="0%";
+    document.getElementById("Vetement").style.width="0%";
+    document.getElementById("books").style.width="0%";
+    document.getElementById("electro").style.width="0%";
+  }
+  function closeNav4() {
+    document.getElementById("home").style.width="0%";
+  }
