69472eafbbbb1da8fe9eb21736cdb0ef7c6ac97c
Khouloud Bolif <khouloudbolif@gmail.com>
2023-01-25 02:53:57 +0100

    update of overflow-x

--- projet php/login.php
+++ projet php/login.php
@@ -1,17 +1,23 @@
 <?php 
 //connexion en base donner de login
-try{
-$conn=new PDO('mysqli:host=localhost;dbname=login',"root","");}
-catch(Exception $e){
-    die('erreur :'.$e->getMessage());
-}
-$nom=$_POST('name');
-$prenom=$_POST('prenom');$email=$_POST('email');$date=$_POST('date');$telephone=$_POST('telephone');
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$conn = new PDO("mysql:host=$servername;dbname=login",
+    $username, $password) ;
+
+
+$nom=$_POST['nom'];
+$prenom=$_POST['prenom'];
+$email=$_POST['email']; 
+$date=$_POST['date'];
+$telephone=$_POST['telephone'];
 //insertion donnes au bd
-$sql='
-INSERT INTO client(nom,prenom,email,telephone,date) VALUES(?,?,?,?)
-';
-$requet=$conn->prepare($sql);
+
+$requet=$conn->prepare('INSERT INTO client(nom,prenom,email,telephone,date) VALUES(?,?,?,?,?)');
 $requet->execute(array($nom,$prenom,$email,$telephone,$date));
 
 
